894f7578e096cd800e5b048f2e722c7e86bf6f09
Mélanie Journé <lostarkaddicted@gmail.com>
2024-08-13 11:12:20 +0200

    Updates

---
+++ static/js/198.347b1a48.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkraid_la_addicted=self.webpackChunkraid_la_addicted||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.347b1a48.chunk.js.map
